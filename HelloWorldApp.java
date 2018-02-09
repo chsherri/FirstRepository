e27965aa91d81103d35f7085628d2181bfe92113
Cole Sherrill <chsherri@email.uark.edu>
2018-02-09 08:50:57 -0600

    Tracker 1. Add who is saying 'hello'.

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -3,5 +3,6 @@ class HelloWorldApp
 	public static void main(String[] arsg)
 		{
 		System.out.println("Hello World!"); //Display the string.
+		System.out.println("Says George."); // Tracker 1 - Display ho is saying 'hello'.
 		}
 	}
